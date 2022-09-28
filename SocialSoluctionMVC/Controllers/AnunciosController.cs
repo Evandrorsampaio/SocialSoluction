0b7cea175f6da4eb4df3062f7234ef99d915a9f8
EVANDRO ROSA SAMPAIO <4976960+Evandrorsampaio@users.noreply.github.com>
2022-09-28 19:14:18 -0300

    Telas básicas.

---
+++ SocialSoluctionMVC/Controllers/AnunciosController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SocialSoluctionMVC.Data.Context;
+using SocialSoluctionMVC.Entities;
+
+namespace SocialSoluctionMVC.Controllers
+{
+    public class AnunciosController : Controller
+    {
+        private readonly SocialSoluctionContext _context;
+
+        public AnunciosController(SocialSoluctionContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Anuncios
+        public async Task<IActionResult> Index()
+        {
+            var socialSoluctionContext = _context.Anuncios.Include(a => a.Cliente);
+            return View(await socialSoluctionContext.ToListAsync());
+        }
+
+        // GET: Anuncios/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var anuncio = await _context.Anuncios
+                .Include(a => a.Cliente)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (anuncio == null)
+            {
+                return NotFound();
+            }
+
+            return View(anuncio);
+        }
+
+        // GET: Anuncios/Create
+        public IActionResult Create()
+        {
+            ViewData["ClienteId"] = new SelectList(_context.Clientes, "Id", "CPFCNPJ");
+            return View();
+        }
+
+        // POST: Anuncios/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Tipo,ClienteId,Valor,Publicacao,Descricao,CEP,Logradouro,Complemento,Bairro,Localidade,UF,Id")] Anuncio anuncio)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(anuncio);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ClienteId"] = new SelectList(_context.Clientes, "Id", "CPFCNPJ", anuncio.ClienteId);
+            return View(anuncio);
+        }
+
+        // GET: Anuncios/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var anuncio = await _context.Anuncios.FindAsync(id);
+            if (anuncio == null)
+            {
+                return NotFound();
+            }
+            ViewData["ClienteId"] = new SelectList(_context.Clientes, "Id", "CPFCNPJ", anuncio.ClienteId);
+            return View(anuncio);
+        }
+
+        // POST: Anuncios/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Tipo,ClienteId,Valor,Publicacao,Descricao,CEP,Logradouro,Complemento,Bairro,Localidade,UF,Id")] Anuncio anuncio)
+        {
+            if (id != anuncio.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(anuncio);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AnuncioExists(anuncio.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ClienteId"] = new SelectList(_context.Clientes, "Id", "CPFCNPJ", anuncio.ClienteId);
+            return View(anuncio);
+        }
+
+        // GET: Anuncios/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var anuncio = await _context.Anuncios
+                .Include(a => a.Cliente)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (anuncio == null)
+            {
+                return NotFound();
+            }
+
+            return View(anuncio);
+        }
+
+        // POST: Anuncios/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var anuncio = await _context.Anuncios.FindAsync(id);
+            _context.Anuncios.Remove(anuncio);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AnuncioExists(int id)
+        {
+            return _context.Anuncios.Any(e => e.Id == id);
+        }
+    }
+}
